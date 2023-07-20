1309424cbece8453846bb9b1174b064e38768ff4
knull-a <kenjopa6@gmail.com>
2023-07-20 19:44:50 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl bg-slate-500">Hello world</div>
+      <div className="rounded bg-black w-11 h-11"></div>
+    </>
+  );
+}
+
+export default App;
