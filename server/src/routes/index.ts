d9f57ad89d41d14841325b2788cf271c80967141
Bakhtiyar Kainolla <kenjopa6@gmail.com>
2023-11-13 12:27:25 +0500

    add middleware to requests

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,9 +2,10 @@ import { Express } from "express";
 import notesRoute from "./notes";
 import labelRoute from "./label";
 import authRoute from "./auth";
+import authMiddleware from "../middlewares/authMiddleware";
 
 export function initRoutes(app: Express) {
   app.use("/api/v1/auth", authRoute);
-  app.use("/api/v1/notes", notesRoute);
-  app.use("/api/v1/labels", labelRoute);
+  app.use("/api/v1/notes", authMiddleware, notesRoute);
+  app.use("/api/v1/labels", authMiddleware, labelRoute);
 }
